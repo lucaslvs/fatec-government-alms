776a227937428a8dce3d9a4bc14179d3bb867c41
Phelps <phelpsmemo@gmail.com>
2017-10-04 00:52:38 -0300

    Adição da função de plotar os estados

--- plot.py
+++ plot.py
@@ -1,3 +1,9 @@
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.io.json import json_normalize
+
 def plot_all_years(json_data):
     pass
 
@@ -11,4 +17,27 @@ def plot_year(json_data, year):
 
 
 def plot_state(json_data, state):
-    pass
+    with open('dados.json') as dados:
+        data = json.load(dados)
+        data2 = data
+        data = json_normalize(data)
+    
+    valores = []
+    anos = []
+    
+    for k in data:
+        if k[10:-5] == state.upper():
+           valores.append(data[k][0])
+           anos.append(int(k[-4:]))
+           name = k
+    
+    # Média de crescimento
+    media = np.median(valores)
+    
+    plt.title('Valores totais gastos em: ' + name[10:-5])
+    plt.xlabel('Anos')
+    plt.ylabel('Gastos (Bilhões)')
+    plt.plot(anos, valores)
+    media_patch = mpatches.Patch(label='Média de crescimento anual (Milhões): ' + str(media)[0:3])
+    plt.legend(handles=[media_patch])
+    plt.show()
